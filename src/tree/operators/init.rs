be16b1cb4b2f58d75986e5f96f68d417a4f228b4
kpindur <kotaro.pindur@gmail.com>
2024-08-02 15:55:41 +0900

    feat(tree.operators.init): add skeleton of an `Initializer`

--- src/tree/operators/init.rs
+++ src/tree/operators/init.rs
@@ -1,23 +1,47 @@
+use std::collections::HashMap;
+use rand::Rng;
+
 use crate::common::traits::Initializer;
 use crate::tree::core::individual::TreeGenotype;
+use crate::tree::core::sampler::{OperatorSampler, Sampler};
 
-pub struct Full {}
+pub struct Grow {
+    min_depth: usize,
+    max_depth: usize
+}
 
-impl Initializer<TreeGenotype> for Full {
-    fn initialize(&self) -> TreeGenotype {
-        // Pseudocode
-        // 0: initialize stack (Vec<bool> should suffice)
-        // 1: initialize structure, e.g. Vec<id>
-        // 2: push value onto stack
-        // 3: while !stack.is_empty()
-        // 4:   pop from stack and initialize (id, arity) pair
-        // 5:   for arity:
-        // 6:       push value onto stack
-        // 7:   end
-        // 8: end
-        // 9: construct TreeGenotype via associated function (e.g. new)
-        // 10: return created TreeGenotype
-        //
-        todo!()
+impl Grow {
+    pub fn new(min_depth: usize, max_depth: usize) -> Self {
+        return Self { min_depth, max_depth };
+    }
+    
+
+}
+
+impl Initializer<TreeGenotype> for Grow {
+    fn initialize<R: Rng>(&self, rng: &mut R, sampler: OperatorSampler) -> TreeGenotype {
+        let mut stack: Vec<(usize, usize)> = Vec::new();
+        let mut tree: TreeGenotype = TreeGenotype::new(Vec::new(), HashMap::new());
+        
+        let mut root: usize = 0;
+        let (node_id, node_arity) = sampler.sample(rng);
+
+        tree.arena_mut().push(node_id);
+        for _ in 0..node_arity {
+            stack.push((root, 1));
+        }
+        
+        while let Some((parent, depth)) = stack.pop() {
+            root += 1;
+            let (node_id, node_arity) = sampler.sample(rng);
+
+            tree.arena_mut().push(node_id);
+            tree.children_mut().entry(parent).or_default().push(root);
+            for _ in 0..node_arity {
+                stack.push((root, depth+1));
+            }
+        }
+
+        return tree;
     }
 }
