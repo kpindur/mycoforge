ee28abc708bb6faa5d69631302289bb8c9e53da0
kpindur <kotaro.pindur@gmail.com>
2024-08-04 15:45:24 +0900

    feat(tree.core.init): implement `Grow.initialize()`

--- src/tree/operators/init.rs
+++ src/tree/operators/init.rs
@@ -23,8 +23,15 @@ impl Initializer<TreeGenotype> for Grow {
         let mut stack: Vec<(usize, usize)> = Vec::new();
         let mut tree: TreeGenotype = TreeGenotype::new(Vec::new(), HashMap::new());
         
+        let (term_set, func_set) = (sampler.sampler_with_arity(0, 0), sampler.sampler_with_arity(1, 2)); // max_arity should depend on sample, e.g., sampler.arity().iter().max()
+
         let mut root: usize = 0;
-        let (node_id, node_arity) = sampler.sample(rng);
+        let (node_id, node_arity) = 
+            if self.max_depth == 0 {
+                term_set.sample(rng)
+            } else {
+                func_set.sample(rng)
+            };
 
         tree.arena_mut().push(node_id);
         for _ in 0..node_arity {
@@ -33,7 +40,14 @@ impl Initializer<TreeGenotype> for Grow {
         
         while let Some((parent, depth)) = stack.pop() {
             root += 1;
-            let (node_id, node_arity) = sampler.sample(rng);
+            let (node_id, node_arity) = 
+                if depth == self.max_depth {
+                    term_set.sample(rng)
+                } else if depth < self.min_depth {
+                    func_set.sample(rng)
+                } else {
+                    [&term_set, &func_set].choose(rng).unwrap().sample(rng)
+                };
 
             tree.arena_mut().push(node_id);
             tree.children_mut().entry(parent).or_default().push(root);
