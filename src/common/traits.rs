7d799063762bb5c7d27701e22f4a9f8b22e6d533
kpindur <kotaro.pindur@gmail.com>
2024-11-15 06:27:04 +0900

    feat(traits): refactor `Evaluator` trait
    Add Data trait for defining custom `Dataset` classes.
    Evaluator uses associated type (`Data`) implementation, and needs both
    data (read-only) and map of operators (hash map of strings to arities
    and functions).

--- src/common/traits.rs
+++ src/common/traits.rs
@@ -1,6 +1,7 @@
 use std::fmt::Display;
 
 use rand::Rng;
+use std::collections::HashMap;
 
 use crate::tree::core::sampler::OperatorSampler;
 
@@ -18,19 +19,26 @@ pub trait Crossoverer<G: Genotype> {
     fn variate<R: Rng>(&self, rng: &mut R, parent1: &G, parent2: &G, sampler: &OperatorSampler) -> Vec<G>;
 }
 
+pub trait Data {
+    fn data_train(&self) -> (usize, &Vec<Vec<f64>>);
+    fn data_test(&self)  -> (usize, &Vec<Vec<f64>>);
+}
+
+pub trait Evaluator<G: Genotype> {
+    type D: Data;
+
+    fn evaluate(&self, tree: &G, data: &Self::D, map: HashMap<String, (usize, fn(&[&[f64]])-> Vec<f64>)>) -> f64;
+}
+
 pub trait Selector<G: Genotype> {
     fn select(&self, population: &[G]) -> G;
 }
 
-pub trait Evaluator {
-    fn evaluate(&self) -> f64;
-}
 
 pub trait Individual {
     type G: Genotype;
-    type E: Evaluator;
 
     fn new(genotype: Self::G) -> Self;
     fn genotype(&self) -> &Self::G;
-    fn phenotype(&self) -> &Self::E;
+    fn phenotype(&self) -> f64;
 }
