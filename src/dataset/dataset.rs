7d88ace3815c8a5a3b8e48d34d445c2994f5ddd1
kpindur <kotaro.pindur@gmail.com>
2024-12-09 22:54:44 +0900

    test(dataset): remove tests

--- src/dataset/dataset.rs
+++ src/dataset/dataset.rs
@@ -68,27 +68,3 @@ impl Data for Dataset {
     fn data_test(&self)   -> (usize, &Vec<Vec<f64>>) { return (self.no_dims, self.test_data()); }
     fn data_train(&self)  -> (usize, &Vec<Vec<f64>>) { return (self.no_dims, self.train_data()); }
 }
-
-#[cfg(test)]
-pub mod tests {
-    use super::*;
-    use rand::rngs::StdRng;
-    use rand::SeedableRng;
-
-    #[test]
-    fn test_creation() {
-        let mut rng = StdRng::seed_from_u64(102);
-
-        let feature_names = ["x", "y", "z"].iter().map(|v| v.to_string()).collect::<Vec<String>>();
-        let data_x = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
-        let data_y = vec![0.0, -1.0, -2.0, -3.0, -4.0, -5.0];
-        let data_z = vec![0.0, 0.1, 0.2, 0.3, 0.4, 0.5];
-
-        let train_ratio = 0.1;
-        let dataset = Dataset::from_vector(&mut rng, feature_names, vec![data_x, data_y, data_z], train_ratio);
-        println!("Dataset.test_data: {:?}", dataset.test_data());
-        println!("Dataset.train_data: {:?}", dataset.train_data());
-        // Any sensible test case?
-        //assert_eq!(&data_out, dataset.data());
-    }
-}
