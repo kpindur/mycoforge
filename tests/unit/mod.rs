b27e32f6f47bcc9262a057a0a240329ae468967d
kpindur <kotaro.pindur@gmail.com>
2024-12-09 22:44:58 +0900

    test(tests.unit.mod): include tests in the project

--- tests/unit/mod.rs
+++ tests/unit/mod.rs
@@ -1,1 +1,12 @@
 mod tree_tests;
+mod individual_tests;
+
+mod functions_test;
+mod sampler_tests;
+
+mod init_tests;
+mod mutation_tests;
+mod crossover_tests;
+mod evaluate_tests;
+
+mod select_tests;
