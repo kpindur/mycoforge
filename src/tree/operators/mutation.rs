7feaaf4b40ebabbad91945a33c68d687d73b6dab
kpindur <kotaro.pindur@gmail.com>
2024-12-14 08:56:24 +0900

    feat(tree.operators): change `subtitute` to a private function
    Two reasons for this change: (i) there's no need for it to exist inside
    SubtreeMutation, because it doesn't require direct access to fields or
    other methods in the struct; and (ii) other mutation schemes will (most
    likely) use it as well.

--- src/tree/operators/mutation.rs
+++ src/tree/operators/mutation.rs
@@ -9,6 +9,17 @@ use super::init::Grow;
 use super::errors::MutationError;
 use log::{info, error, debug};
 
+fn substitute(individual: &TreeGenotype, subtree: &TreeGenotype, mutation_point: usize) 
+    -> Vec<String> {
+    let mutation_end: usize = individual.subtree(mutation_point);
+
+    let mut new_arena = individual.arena()[0..mutation_point].to_vec();
+    new_arena.extend(subtree.arena().iter().cloned());
+    new_arena.extend(individual.arena()[mutation_end+1..].iter().cloned());
+
+    return new_arena;
+}
+
 pub struct SubtreeMutation {
     probability: f64,
     depth_limits: (usize, usize)
@@ -27,19 +38,8 @@ impl SubtreeMutation {
             error!("Attempted to crate SubtreeMutation with invalid probability: {}", probability);
             return Err(MutationError::InvalidProbability(probability));
         }
-        info!("Created SubtreeMutation operator with probability {}", probability);
+        info!("Created SubtreeMutation operator with probability {} and depth limits ({}, {})", probability, depth_limits.0, depth_limits.1);
         return Ok(Self { probability, depth_limits });
-    }
-
-    fn substitute(individual: &TreeGenotype, subtree: &TreeGenotype, mutation_point: usize) 
-        -> Vec<String> {
-        let mutation_end: usize = individual.subtree(mutation_point);
-
-        let mut new_arena = individual.arena()[0..mutation_point].to_vec();
-        new_arena.extend(subtree.arena().iter().cloned());
-        new_arena.extend(individual.arena()[mutation_end+1..].iter().cloned());
-
-        return new_arena;
     }
 }
 
@@ -53,10 +53,10 @@ impl Mutator<TreeGenotype> for SubtreeMutation {
         let mutation_point: usize = rng.gen_range(0..individual.arena().len());
         
         let init_scheme = Grow::new(self.depth_limits.0, self.depth_limits.1);
-        let subtree: TreeGenotype = init_scheme.initialize(rng, sampler);
+        let subtree = init_scheme.initialize(rng, sampler);
         debug!("Generated subtree of size {} at point {}", subtree.arena().len(), mutation_point);
         
-        let arena = Self::substitute(individual, &subtree, mutation_point);
+        let arena = substitute(individual, &subtree, mutation_point);
         let mut tree = TreeGenotype::with_arena(arena);
         *tree.children_mut() = tree.construct_children(sampler);
         
