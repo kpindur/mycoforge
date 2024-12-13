49a782c8b069a7ab4860d474fe6e3e5b3fdd11e5
kpindur <kotaro.pindur@gmail.com>
2024-12-14 08:59:31 +0900

    feat(tree.operators): implemented `SizeFairMutation`

--- src/tree/operators/mutation.rs
+++ src/tree/operators/mutation.rs
@@ -64,3 +64,61 @@ impl Mutator<TreeGenotype> for SubtreeMutation {
         return tree.clone();
     }
 }
+
+pub struct SizeFairMutation {
+    probability: f64,
+    dynamic_limit: bool
+}
+
+impl Default for SizeFairMutation {
+    fn default() -> Self {
+        debug!("Creating default SizeFairMutation with probability {} and dynamic limit {}", 0.1, false);
+        return Self::new(0.1, false).expect("Failed to create default SizeFairMutation!");
+    }
+}
+
+impl SizeFairMutation {
+    pub fn new(probability: f64, dynamic_limit: bool) -> Result<Self, MutationError> {
+        if !(0.0..=1.0).contains(&probability) {
+            error!("Attempted to create SizeFairMutation with invalid probability: {}", probability);
+            return Err(MutationError::InvalidProbability(probability));
+        }
+        info!("Created SizeFairMutation operator with probability {} and dynamic limit {}", probability, dynamic_limit);
+        return Ok(Self { probability, dynamic_limit });
+    }
+
+    fn calculate_depths(&self, tree: &TreeGenotype, mutation_point: usize) -> (usize, usize) {
+        let tree_size = if self.dynamic_limit {
+            let subtree_end = tree.subtree(mutation_point);
+            (subtree_end - mutation_point) as f64
+        } else {
+            tree.arena().len() as f64
+        };
+
+        let depth_min = (tree_size / 2.0).floor().log2() as usize;
+        let depth_max = (tree_size * 1.5).ceil().log2() as usize;
+
+        return (depth_min, depth_max);
+    }
+}
+
+impl Mutator<TreeGenotype> for SizeFairMutation {
+    fn variate<R: Rng>(&self, rng: &mut R, individual: &TreeGenotype, sampler: &OperatorSampler) -> TreeGenotype {
+        if rng.gen::<f64>() > self.probability {
+            debug!("Skipping mutation");
+            return individual.clone();
+        }
+
+        let mutation_point = rng.gen_range(0..individual.arena().len());
+        let depth_limits = self.calculate_depths(individual, mutation_point);
+
+        let init_scheme = Grow::new(depth_limits.0, depth_limits.1);
+        let subtree = init_scheme.initialize(rng, sampler);
+
+        let arena = substitute(individual, &subtree, mutation_point);
+        let mut tree = TreeGenotype::with_arena(arena);
+        *tree.children_mut() = tree.construct_children(sampler);
+
+        return tree.clone();
+    }
+}
