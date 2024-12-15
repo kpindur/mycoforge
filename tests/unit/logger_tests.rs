cb417d4716c4ddf1fd4754dcfcfc035b6d8bb8c6
kpindur <kotaro.pindur@gmail.com>
2024-12-16 08:08:55 +0900

    feat(tests.unit): add test for `PostgresLogger`

--- tests/unit/logger_tests.rs
+++ tests/unit/logger_tests.rs
@@ -1,6 +1,10 @@
 use std::fs;
 use log::LevelFilter;
+use mycoforge::dataset::logger::LogEntries;
+use mycoforge::dataset::logger::Logger;
 use mycoforge::dataset::logger::SimpleLogger;
+
+use std::sync::{Arc, Mutex};
 
 #[test]
 fn test_file_logging() {
@@ -16,4 +20,54 @@ fn test_file_logging() {
     assert!(contents.contains("test message"));
 
     fs::remove_file(test_file).unwrap_or_else(|_| panic!("Failed to delete file {}", test_file));
+}
+
+use mycoforge::dataset::logger::PostgresLogger;
+use std::thread;
+use std::time::Duration;
+use log::{Level, Record, Log};
+
+#[test]
+fn test_postgres() {
+    let entries = Arc::new(Mutex::new(LogEntries::default()));
+    let logger = Logger::new(entries.clone(), LevelFilter::Debug);
+    let db = PostgresLogger::new("test", entries.clone()).expect("Failed to connect to Postgresql!");
+
+    thread::spawn(move || {
+        if let Err(e) = db.run(Duration::from_secs(1)) { eprintln!("DB error in test: {}", e) }
+    });
+
+    logger.log(&Record::builder()
+        .args(format_args!("Test message 1"))
+        .level(Level::Info)
+        .target("test")
+        .file(Some(file!()))
+        .line(Some(line!()))
+        .module_path(Some(module_path!()))
+        .build()
+    );
+
+    logger.log(&Record::builder()
+        .args(format_args!("Test message 2"))
+        .level(Level::Warn)
+        .target("test")
+        .file(Some(file!()))
+        .line(Some(line!()))
+        .module_path(Some(module_path!()))
+        .build()
+    );
+
+    thread::sleep(Duration::from_secs(2));
+
+    let mut client = PostgresLogger::new("test", entries.clone()).expect("Failed to connect to Postgresql!");
+
+    let rows = client.db_client_mut().query(
+        "select * from test order by timestamp", &[]
+    ).expect("Successfully retrieved rows from database!");
+
+    println!("Retrieved {} rows!", rows.len());
+    assert_eq!(rows.len(), 2,
+        "Retreived more rows than should have! Expected {}, found {}", 2, rows.len()
+    );
+    client.db_client_mut().query("drop table if exists test", &[]).expect("Successfully dropped table: test");
 }
