c35fa3c0522533d8b0d4e97be05532412af4aa14
kpindur <kotaro.pindur@gmail.com>
2024-12-18 08:27:57 +0900

    docs(tree.operators): add basic documentation

--- src/tree/operators/select.rs
+++ src/tree/operators/select.rs
@@ -1,3 +1,8 @@
+//! Selection methods for Genetic Programming
+//!
+//! This module provides selection operators for GP algorithms designed for selecting individuals
+//! based on their fitness values. Also serves as a template for custom selection operators.
+
 use core::panic;
 
 use rand::Rng;
@@ -6,11 +11,31 @@ use rand::seq::SliceRandom;
 use crate::common::traits::{Individual, Selector};
 use crate::tree::core::{tree::TreeGenotype, individual::TreeIndividual};
 
+/// Tournament selection operator that selects best individual from random subset.
+///
+/// # Fields
+/// * `tournament_size: usize` - Number of individuals randomly sampled for tournament
+///
+/// # Examples
+/// ```
+/// use mycoforge::tree::operators::select::TournamentSelection;
+///
+/// let selection = TournamentSelection::new(7);
+///
+/// assert_eq!(7, selection.tournament_size(),
+///     "Tournament sizes do not match! Expected {}, found {}",
+///     7, selection.tournament_size()
+/// );
+/// ```
 pub struct TournamentSelection {
     tournament_size: usize,
 }
 
 impl TournamentSelection {
+    /// Creates new TournamentSelection operator.
+    ///
+    /// # Arguments
+    /// * `tournament_size: usize` - number of individuals in tournament
     pub fn new(tournament_size: usize) -> Self { return Self { tournament_size }; }
 
     pub fn tournament_size(&self) -> usize { return self.tournament_size; }
