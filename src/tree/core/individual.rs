f826dc603ad1e61ffcb532d5cdd600cec2f3db78
kpindur <kotaro.pindur@gmail.com>
2024-12-18 10:02:55 +0900

    docs(tree.core): add basic documentation to TreeIndividual

--- src/tree/core/individual.rs
+++ src/tree/core/individual.rs
@@ -1,5 +1,35 @@
+//! Core individual structure for evolutionary algorithms.
+//!
+//! This module provides the [`TreeIndividual`] structure that combines genotype with its fitness
+//! value.
 use crate::common::traits::{Genotype, Individual};
 
+/// Individual representation that pairs genotype with its fitness value.
+///
+/// # Type Parameters
+/// * `G: Genotype`- type implementing [`Genotype`][`crate::common::traits::Genotype`] trait
+///
+/// # Fields
+/// * `genotype: G` - [`Genotype`][`crate::common::traits::Genotype`] representation
+/// * `fitness: f64` - fitness value
+///
+/// # Examples
+/// ```
+/// use mycoforge::common::traits::Individual;
+/// use mycoforge::tree::core::individual::TreeIndividual;
+/// use mycoforge::tree::core::tree::TreeGenotype;
+///
+/// let individual = TreeIndividual::new(TreeGenotype::default(), 0.0);
+///
+/// assert!(individual.genotype().arena().is_empty() &&
+///     individual.genotype().children().is_empty(),
+///     "Default individual should be empty!"
+/// );
+/// assert!(0.0 - individual.phenotype() < 1e-10,
+///     "Default individuals fitness should be {}, found {}",
+///     0.0, individual.phenotype()
+/// );
+/// ```
 #[derive(Clone)]
 pub struct TreeIndividual<G: Genotype> {
     genotype: G,
@@ -7,6 +37,11 @@ pub struct TreeIndividual<G: Genotype> {
 }
 
 impl<G: Genotype> TreeIndividual<G> {
+    /// Creates new individual with given genotype and fitness.
+    ///
+    /// # Arguments
+    /// * `genotype: G` - genotype representation
+    /// * `fitness: f64` - fitness value
     pub fn new(genotype: G, fitness: f64) -> Self {
         return Self { genotype, fitness };
     }
