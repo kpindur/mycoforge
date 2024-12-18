03956fa2a488546fa1f4d939cc7da1f9fa576a26
kpindur <kotaro.pindur@gmail.com>
2024-12-19 08:06:05 +0900

    docs(tree.core): add basic documentation to the module

--- src/tree/core/mod.rs
+++ src/tree/core/mod.rs
@@ -1,4 +1,8 @@
-//pub mod sampler;
+//! Core structures for tree-based Genetic Programming.
+//!
+//! This module provides:
+//! - [`tree`] - Tree genotype representation using arena
+//! - [`individual`] - Tree individual combining genotype and fitness
 
 pub mod tree;
 pub mod individual;
