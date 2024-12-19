66d5493ba1ed9425ee47563ba7b2ff03fdebef15
kpindur <kotaro.pindur@gmail.com>
2024-12-19 22:48:37 +0900

    docs(operators): add basic documentation to the module

--- src/operators/mod.rs
+++ src/operators/mod.rs
@@ -1,3 +1,13 @@
+//! Common operators and operator sets for Genetic Programming.
+//!
+//! This module provides:
+//! - [`sampler`] - Core functionality for weighted random sampling of operators with arity
+//! constraints
+//! - [`functions`] - Common vectorized functions for symbolic regression (arithmetic,
+//! trigonometric, etc.)
+//! - [`set`] - Management of operator sets including builder batter for creating valid sets and
+//! sampling functionality.
+
 pub mod functions;
 
 pub mod set;
