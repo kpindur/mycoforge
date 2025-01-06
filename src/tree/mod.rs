b0b198c40b2c3e638862b752e06b9bda32e56971
kpindur <kotaro.pindur@gmail.com>
2025-01-06 09:51:57 +0900

    feat(tree): flatten tree gp related imports
    Should be easier to import tree related methods.

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -10,3 +10,13 @@ pub mod core;
 pub mod operators;
 
 pub mod fitness;
+
+pub use self::core::tree::TreeGenotype;
+pub use self::core::individual::TreeIndividual;
+pub use self::operators::{
+    init::*,
+    mutation::*,
+    crossover::*,
+    select::*
+};
+pub use self::fitness::evaluate::*;
