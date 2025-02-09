ecec9d484e986394c34ff35841b88478f9b31b31
kpindur <kotaro.pindur@gmail.com>
2025-02-09 19:29:22 +0900

    clean(dataset): clean up project structure

--- src/dataset/mod.rs
+++ src/dataset/mod.rs
@@ -4,11 +4,8 @@
 //! - [`core`] - Core dataset structures and traits
 //! - [`error`] - Dataset-related error types
 //! - [`loaders`] - Data loading utilities
-//! - [`logger`] - Dataset logging functionality
 
 pub mod error;
 pub mod loaders;
 
 pub mod core;
-
-pub mod logger;
