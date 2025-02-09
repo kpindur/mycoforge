143843e2b610ad9951814672855ca92e1d465e3d
kpindur <kotaro.pindur@gmail.com>
2025-02-09 19:30:33 +0900

    fix(dataset): re-add `logger.rs` to the project

--- src/dataset/mod.rs
+++ src/dataset/mod.rs
@@ -4,8 +4,11 @@
 //! - [`core`] - Core dataset structures and traits
 //! - [`error`] - Dataset-related error types
 //! - [`loaders`] - Data loading utilities
+//! - [`logger`] - logging utilities
 
 pub mod error;
 pub mod loaders;
 
 pub mod core;
+
+pub mod logger;
